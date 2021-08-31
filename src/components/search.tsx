ba4bb7c3ab368dfbe8d5b619547c42ee0a8c8328
Anna Guzova <36640991+anutaguzova@users.noreply.github.com>
2021-08-31 13:10:20 +0300

    feat: add search

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,13 +1,10 @@
 export function Search() {
-  // const [filter, setFilter] = useState('');
-
-  // const searchText = (event) => {
-  //     setFilter(event.target.value)
-  // }
   return (
     <div className="search">
       <input type="text" className="searchTerm" placeholder="Search for..." />
-      <button type="submit" className="searchButton"></button>
+      <button type="submit" className="searchButton">
+        Search
+      </button>
     </div>
   );
 }
