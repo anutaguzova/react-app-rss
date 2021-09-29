2d94111d3f2d733f8ccc09de8d1648289a27350d
Anna Guzova <36640991+anutaguzova@users.noreply.github.com>
2021-09-30 01:47:46 +0300

    feat: add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Approuter from './components/Approuter';
 import Navbar from './components/Navbar';
+import { store } from './redux';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <main>
-        <div className="App">
-          <Approuter />
-        </div>
-      </main>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <div className="App">
+            <Approuter />
+          </div>
+        </main>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
---
+++ src/redux/index.tsx
@@ -0,0 +1,10 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({ gameSetting: setGameSetting, });
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
